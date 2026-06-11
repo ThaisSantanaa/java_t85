ab177618bb7cda9e2928f8a0e57c46246398f777
ThaisSantanaa <thaislimasantana9@gmail.com>
2026-06-11 12:28:16 -0300

    aula_01_variaveis

---
+++ aula_01_variaveis/src/aula_01_variaveis/Variaveis.java
@@ -0,0 +1,48 @@
+package aula_01_variaveis;
+
+import java.util.Scanner;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int numero1 = 10;
+		int numero2 = 5;
+		float numero3, numero4;
+		float teste = 4.5f;
+		String nome;
+		char opcao = 'N';
+		
+		System.out.println("variavel numero1 = " + numero1);
+		
+		System.out.println("Variavel numero2 = " + numero2);
+		
+		System.out.printf("%d + %d = %d\n", numero1, numero2, numero1 + numero2);
+		
+		System.out.print("Digite o primeiro valor real: ");
+		numero3 = leia.nextFloat();
+		
+		System.out.print("\nDigite o segundo valor real: ");
+		numero4 = leia.nextFloat();
+		
+		System.out.printf("%.2f x %.2f = %.2f", numero3, numero4, numero3 * numero4);
+		
+		System.out.print("\nDigite o seu nome: ");
+		leia.skip("\\R?");
+		nome = leia.nextLine();
+		
+		System.out.println("Olá" + nome +", Seja bem-vindo(a)!");
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
